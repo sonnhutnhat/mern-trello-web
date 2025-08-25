ea702b3a8f5344868f49f470f009471a9ce64ada
sonnhutnhat <nguyenkienson3010@gmail.com>
2025-08-25 23:03:40 +0700

    Add CSS Baseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-
+import CssBaseline from '@mui/material/CssBaseline'
 function App() {
   return (
     <>
+      <CssBaseline />
       <div>abc</div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
