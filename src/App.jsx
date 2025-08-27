ea7db5068aacbebe7fa8a1c5c5afcee9fa59d0f4
sonnhutnhat <nguyenkienson3010@gmail.com>
2025-08-27 22:45:46 +0700

    Dark_Light #1

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,25 @@ import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import CssBaseline from '@mui/material/CssBaseline'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
 function App() {
   return (
     <>
+      <ModeToggle />
       <CssBaseline />
       <div>abc</div>
       <Button variant="text">Text</Button>
--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,27 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
-
+import { experimental_extendTheme as extendTheme} from '@mui/material/styles'
+import { teal, deepOrange, cyan, orange} from '@mui/material/colors'
 // Create a theme instance.
-const theme = createTheme({
-  cssVariables: true,
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#556cd6'
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          main: teal[500]
+        },
+        secondary: {
+          main: deepOrange[500]
+        }
+      }
     },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
+    dark: {
+      palette: {
+        primary: {
+          main: cyan[500]
+        },
+        secondary: {
+          main: orange[500]
+        }
+      }
     }
   }
 })
